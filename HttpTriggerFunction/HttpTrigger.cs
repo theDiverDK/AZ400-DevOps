72bc5b04e70873b427fdd41ff386a0c697b18627
Søren Reinke <soren@reinke.dk>
2024-01-25 15:53:00 +0100

    try paths

--- HttpTriggerFunction/HttpTrigger.cs
+++ HttpTriggerFunction/HttpTrigger.cs
@@ -21,6 +21,7 @@ namespace HttpTriggerFunction
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
+            
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
