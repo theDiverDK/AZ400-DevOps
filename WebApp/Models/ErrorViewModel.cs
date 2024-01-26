5b4d2b23685f93a3ae6b69ed0a96163ca772627d
Søren Reinke <sre@ooono.dk>
2024-01-26 19:31:48 +0100

    changed webapp

--- WebApp/Models/ErrorViewModel.cs
+++ WebApp/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace WebApp.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
