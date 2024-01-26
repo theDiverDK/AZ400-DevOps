a5b3967d16895cb861ecc0f12991434825ae62dd
Søren Reinke <soren@reinke.dk>
2024-01-26 11:02:08 +0100

    code changes

--- Functions/HttpTriggerFunction/HttpTrigger.cs
+++ Functions/HttpTriggerFunction/HttpTrigger.cs
@@ -20,11 +20,10 @@ namespace HttpTriggerFunction
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
-            
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            name ??= data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
