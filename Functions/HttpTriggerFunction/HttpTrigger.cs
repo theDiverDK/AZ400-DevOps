548fa1f1656a3bffe00533539f118956a8710294
Søren Reinke <sre@ooono.dk>
2024-02-01 17:24:30 +0100

    new function

--- Functions/HttpTriggerFunction/HttpTrigger.cs
+++ Functions/HttpTriggerFunction/HttpTrigger.cs
@@ -28,7 +28,7 @@ namespace HttpTriggerFunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. Det virker :-)";
 
             return new OkObjectResult(responseMessage);
         }
